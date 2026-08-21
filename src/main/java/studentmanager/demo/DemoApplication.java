5e4e8984c26fc4c7ae1d14ca93a1504ef42dbd1f
son108882 <son108882@donga.edu.vn>
2026-08-21 19:46:17 +0700

    initial commit

---
+++ src/main/java/studentmanager/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package studentmanager.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+
+@SpringBootApplication
+public class DemoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.print("hello");
+	}
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot API";
+    }
+
+}
